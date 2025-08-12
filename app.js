c9c4580ab9271447d45e4d395ad2e9afb64b049a
Tony Tony <tonytony25236@users.noreply.github.com>
2025-08-12 18:42:04 +0000

    add dep

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 80
+const Example = require('@wkl-training/ascpc-npm-example');
+
+const example = new Example();
+process.env.SECRET_KEY = '1234567890';
+process.env.DB_USER = 'ascpc-fake-user';
+process.env.DB_PASSWORD = 'ascpc-fake-password';
 
 app.get('/', (req, res) => {
-  res.send('Uber pwnage!')
+  res.send(example.sayHello())
 })
 
 app.listen(port, () => {
