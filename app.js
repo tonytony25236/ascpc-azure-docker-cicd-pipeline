1c95166d2109a352b4a89a308af6d8029de54306
tonytony25236 <225789205+tonytony25236@users.noreply.github.com>
2025-08-12 15:59:25 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Welcome to your ASCPC web app!')
+  res.send('Uber pwnage!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+
 })
